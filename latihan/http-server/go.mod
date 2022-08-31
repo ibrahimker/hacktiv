31718f6824cee3f8326b90119d68ccb1a7240eff
Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>
2022-08-31 19:27:06 +0700

    add http server example

---
+++ latihan/http-server/go.mod
@@ -0,0 +1,5 @@
+module github.com/ibrahimker/hacktiv/latihan/http-server
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ latihan/http-server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/ibrahimker/hacktiv/latihan/http-server/service"
+)
+
+func main() {
+	var db []*service.User
+	userSvc := service.NewUserService(db)
+	fmt.Println("http server running on localhost:8080")
+	// without mux
+	//http.HandleFunc("/register", userSvc.RegisterHandler)
+	//http.HandleFunc("/user", userSvc.GetUserHandler)
+	//http.ListenAndServe("localhost:8080", nil)
+
+	// with mux
+	r := mux.NewRouter()
+	r.HandleFunc("/register", userSvc.RegisterHandler)
+	r.HandleFunc("/user", userSvc.GetUserHandler)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "localhost:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	_ = srv.ListenAndServe()
+
+}
