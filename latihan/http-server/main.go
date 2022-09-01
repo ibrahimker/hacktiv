f473a793934ebeaa67493a75cf0492872f00822c
Ibrahim Nurandita Isbandiputra <ibrahim.nurandita@wartek.belajar.id>
2022-09-01 20:57:17 +0700

    add gin example

--- latihan/http-server/main.go
+++ latihan/http-server/main.go
@@ -13,16 +13,25 @@ func main() {
 	var db []*service.User
 	userSvc := service.NewUserService(db)
 	fmt.Println("http server running on localhost:8080")
-	// without mux
+	// http handler bawaan
 	//http.HandleFunc("/register", userSvc.RegisterHandler)
 	//http.HandleFunc("/user", userSvc.GetUserHandler)
 	//http.ListenAndServe("localhost:8080", nil)
 
 	// with mux
 	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloWorldHandler)
 	r.HandleFunc("/register", userSvc.RegisterHandler)
 	r.HandleFunc("/user", userSvc.GetUserHandler)
 	r.HandleFunc("/user/{id}", userSvc.GetUserHandler)
+
+	// with gin
+	//r := gin.Default()
+	//r.GET("/hello", gin.WrapF(helloWorldHandler))
+	//r.POST("/register", gin.WrapF(userSvc.RegisterHandler))
+	//r.GET("/user", gin.WrapF(userSvc.GetUserHandler))
+	//r.GET("/user/:id", gin.WrapF(userSvc.GetUserHandler))
+
 	srv := &http.Server{
 		Handler: r,
 		Addr:    "localhost:8080",
@@ -31,5 +40,8 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 	_ = srv.ListenAndServe()
+}
 
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
 }
